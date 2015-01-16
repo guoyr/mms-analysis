19981de73f109acce681ed7662d082f392f2ef42
Robert Guo <robert.guo@10gen.com>
2015-01-16 09:57:19 -0500

    modify script to use hard-coded list of ping information to analyze

--- aggregator.py
+++ aggregator.py
@@ -10,47 +10,18 @@ from pprint import pprint
 
 from pymongo import MongoClient
 
-STRING = 2
-LENGTH = 1
+#grouping modes
+STRING = 2 # group by the subdocument literal string
+LENGTH = 1 # group by the length of the subdocument
 
-#find_query = {"ping.configLockpings.50":{"$exists": True} }
-find_query = {}
-hist_dict = {}
+# list of queries to get a subset of documents
+filterQueries = [{}, {"ping.configLockpings.50":{"$exists": True}}]
 
-if os.path.isfile(".agg_hist"):
-    hist_dict = json.load(open(".agg_hist", "r"))
+# the subdocument to group by
+subdocNames = ["ping.hostinfo.os","ping.hostinfo.os"]
 
-
-def save_hist():
-    json.dump(hist_dict, open(".agg_hist", "w"))
-
-
-def signal_handler(signal, frame):
-    print("saving command history...")
-    save_hist()
-    print("\nexiting...")
-    sys.exit(0)
-
-
-def get_default(fieldName, defaultValue):
-    if fieldName in hist_dict:
-        return hist_dict[fieldName]
-    else:
-        return defaultValue
-
-
-def get_input():
-
-    dbName = raw_input("name of DB: ") or get_default("dbName", "euphonia")
-    collName = raw_input("name of collection: ") or get_default("collName", "pings")
-    subdocName = raw_input("name of sub-document: ") or get_default("subdocName", "ping")
-    mode = raw_input("enter the mode (1 for length or 2 for string):") or 2
-
-    hist_dict["dbName"] = dbName
-    hist_dict["collName"] = collName
-    hist_dict["subdocName"] = subdocName
-
-    return dbName, collName, subdocName, mode
+# grouping mode
+modes = [STRING, STRING]
 
 
 def outputCsv(outFileName, contentTuple):
@@ -63,34 +34,40 @@ def outputCsv(outFileName, contentTuple):
 def main():
 
     client = MongoClient()
-    signal.signal(signal.SIGINT, signal_handler)
-    dbName, collName, subdocName, mode = get_input()
-    save_hist()
 
-    print "DB name: ", dbName
-    print "collection name: ", collName
-    print "subdocument name: ", subdocName
+    dbName = "euphonia"
+    collname = "pings"
 
-    outputDict = defaultdict(int)
+    for filterQuery, subdocName, mode in zip(filterQueries, subdocNames, modes):
 
-    for doc in client[dbName][collName].find(find_query, fields={subdocName: 1}):
-        subdocTree = subdocName.split(".")
-        subdoc = doc
-        try:
-            for key in subdocTree:
-                subdoc = subdoc[key]
-        except Exception:
-            print "document does not have the field " + subdocName
-            continue
+    	print ""
+	    print "DB name: ", dbName
+	    print "collection name: ", collName
+	    print "subdocument name: ", subdocName
+	    print "filter query ", filterQuery
+	    print "mode ", mode
 
-        if mode == LENGTH:
-            outputDict[len(subdoc)] += 1
-        elif mode == STRING:
-            outputDict[subdoc] += 1
+	    outputDict = defaultdict(int)
 
-    sortedOuptutDict = sorted(outputDict.items(), key=operator.itemgetter(1), reverse=True)
-    outputCsv("output", sortedOuptutDict)
-    pprint(sortedOuptutDict)
+	    for doc in client[dbName][collName].find(filterQuery, fields={subdocName: 1}):
+	        subdocTree = subdocName.split(".")
+	        subdoc = doc
+	        try:
+	        	# iterate through the subdocuments
+	            for key in subdocTree:
+	                subdoc = subdoc[key]
+	        except Exception:
+	            print "document does not have the field " + subdocName
+	            continue
+
+	        if mode == LENGTH:
+	            outputDict[len(subdoc)] += 1
+	        elif mode == STRING:
+	            outputDict[subdoc] += 1
+
+	    sortedOuptutDict = sorted(outputDict.items(), key=operator.itemgetter(1), reverse=True)
+	    outputCsv(subdocName, sortedOuptutDict)
+	    pprint(sortedOuptutDict)
 
 if __name__ == '__main__':
     main()
