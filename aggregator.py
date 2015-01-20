d1cd1379ef54e52529777a939c2e39db0433b27a
Robert Guo <robert.guo@10gen.com>
2015-01-20 14:12:22 -0500

    fix indentation

--- aggregator.py
+++ aggregator.py
@@ -40,34 +40,34 @@ def main():
 
     for filterQuery, subdocName, mode in zip(filterQueries, subdocNames, modes):
 
-    	print ""
-	    print "DB name: ", dbName
-	    print "collection name: ", collName
-	    print "subdocument name: ", subdocName
-	    print "filter query ", filterQuery
-	    print "mode ", mode
+        print ""
+        print "DB name: ", dbName
+        print "collection name: ", collName
+        print "subdocument name: ", subdocName
+        print "filter query ", filterQuery
+        print "mode ", mode
 
-	    outputDict = defaultdict(int)
+        outputDict = defaultdict(int)
 
-	    for doc in client[dbName][collName].find(filterQuery, fields={subdocName: 1}):
-	        subdocTree = subdocName.split(".")
-	        subdoc = doc
-	        try:
-	        	# iterate through the subdocuments
-	            for key in subdocTree:
-	                subdoc = subdoc[key]
-	        except Exception:
-	            print "document does not have the field " + subdocName
-	            continue
+        for doc in client[dbName][collName].find(filterQuery, fields={subdocName: 1}):
+            subdocTree = subdocName.split(".")
+            subdoc = doc
+            try:
+                # iterate through the subdocuments
+                for key in subdocTree:
+                    subdoc = subdoc[key]
+            except Exception:
+                print "document does not have the field " + subdocName
+                continue
 
-	        if mode == LENGTH:
-	            outputDict[len(subdoc)] += 1
-	        elif mode == STRING:
-	            outputDict[subdoc] += 1
+            if mode == LENGTH:
+                outputDict[len(subdoc)] += 1
+            elif mode == STRING:
+                outputDict[subdoc] += 1
 
-	    sortedOuptutDict = sorted(outputDict.items(), key=operator.itemgetter(1), reverse=True)
-	    outputCsv(subdocName, sortedOuptutDict)
-	    pprint(sortedOuptutDict)
+        sortedOuptutDict = sorted(outputDict.items(), key=operator.itemgetter(1), reverse=True)
+        outputCsv(subdocName, sortedOuptutDict)
+        pprint(sortedOuptutDict)
 
 if __name__ == '__main__':
     main()
